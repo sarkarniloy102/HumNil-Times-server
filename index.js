5cd1491f1559526895fac3ddd540b6bef816210b
sarkar niloy <sarkarniloy102@gmail.com>
2025-08-25 02:38:01 +0600

    modified structure

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogs = require('./api/blogsData.json')
-const port = process.env.PORT || 5000;
+const express = require('express');
+const cors = require('cors');
+const blogs = require('./api/blogsData.json');  // adjust path if needed
+const app = express();
 
-// middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Blog server is running!")
+  res.send("Blog server is running!");
 });
 
 app.get('/blogs', (req, res) => {
-  res.send(blogs)
-})
+  res.json(blogs);
+});
+
 app.get('/blogs/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  // console.log(id)
   const blog = blogs.filter(b => b.id === id);
-  // console.log(blog)
-  res.send(blog)
-})
+  res.json(blog);
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// 👇 export as Vercel function
+module.exports = app;
